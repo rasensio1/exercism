7adf245e795a862e2d4230ded021d2974a582a69
Ryan Asensio <ryan.asensio@gmail.com>
2017-10-17 15:35:46 -0500

    refactor series with map

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -4,11 +4,11 @@ class Series
   end
 
   def slices(num)
-    num_slices = @string.length - num + 1
-    raise ArgumentError if num_slices <= 0
+    end_index = @string.length - num
+    raise ArgumentError if @string.length < num
 
-    (0..(num_slices -1)).reduce([]) do |sum, i|
-      sum << @string[i..(i + num - 1)]
+    (0..end_index).map do |i|
+      @string[i..(i + num - 1)]
     end
   end
 
