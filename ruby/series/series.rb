46ca6e28c4ed3dc12bafbc281c0a0733814b69e8
Ryan Asensio <ryan.asensio@gmail.com>
2017-10-17 15:20:45 -0500

    refactor series. more idiomatic

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -7,11 +7,9 @@ class Series
     num_slices = @string.length - num + 1
     raise ArgumentError if num_slices <= 0
 
-    output = []
-    (0..(num_slices -1)).each do |i|
-      output << @string[i..(i + num - 1)]
+    (0..(num_slices -1)).reduce([]) do |sum, i|
+      sum << @string[i..(i + num - 1)]
     end
-    output
   end
 
 end
