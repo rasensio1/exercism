4be7c35bc5aa9f46b8c47c0e6a5089f273ac4c15
Ryan Asensio <ryan.asensio@gmail.com>
2017-10-17 15:37:32 -0500

    refactor series - take out local var

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -4,10 +4,8 @@ class Series
   end
 
   def slices(num)
-    end_index = @string.length - num
     raise ArgumentError if @string.length < num
-
-    (0..end_index).map do |i|
+    (0..(@string.length - num)).map do |i|
       @string[i..(i + num - 1)]
     end
   end
