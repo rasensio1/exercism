663302c9c97160774458eca20fc81e440c76a420
Ryan Asensio <ryan.asensio@gmail.com>
2017-10-17 12:04:59 -0500

    accumulate

--- ruby/accumulate/accumulate.rb
+++ ruby/accumulate/accumulate.rb
@@ -1,7 +1,13 @@
 class Array
 
-  def accumulate(arr, &block)
-    arr
+  def accumulate
+    result = []
+    self.each { |e| result << yield(e) }
+    result
   end
 
+end
+
+module BookKeeping
+  VERSION = 1
 end
