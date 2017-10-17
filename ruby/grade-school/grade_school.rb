24d4f2f0bba8d01b39e9d31cac447cfc96b83a6f
Ryan Asensio <ryan.asensio@gmail.com>
2017-10-17 14:58:24 -0500

    make grade_school better

--- ruby/grade-school/grade_school.rb
+++ ruby/grade-school/grade_school.rb
@@ -1,28 +1,22 @@
-require 'pry'
 class School
   attr_accessor :data
 
   def initialize
-    @data = []
+    @data = Hash.new { |h, k| h[k] = [] }
   end
 
   def add(name, grade)
-    data.push([name, grade])
+    data[grade].push(name).sort!
   end
 
   def students(grade)
-    names = data.select do |info|
-      info[1] == grade
-    end.transpose[0]
-    names ||= [] #catch nil results from transpose
-    names.sort
+    data[grade]
   end
 
   def students_by_grade
-    data.chunk{|info| info[1]}.map do |grade_raw|
-      {grade: grade_raw[0],
-       students: grade_raw[1].transpose[0].sort}
-    end.sort_by { |graded| graded[:grade]}
+    data.map do |num, students|
+      {grade: num, students: students}
+    end.sort_by {|line| line[:grade]}
   end
 
 end
