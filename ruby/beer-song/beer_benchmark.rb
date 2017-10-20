80176b6008a109faea8ef853d6c2a1dfbf6f85ce
Ryan Asensio <ryan.asensio@gmail.com>
2017-10-20 13:34:25 -0500

    test lambdas and rename files

--- ruby/beer-song/beer_benchmark.rb
+++ ruby/beer-song/beer_benchmark.rb
@@ -1,15 +1,22 @@
 require 'benchmark'
-require_relative 'beer_song'
+require_relative 'beer_song_procs'
 require_relative 'beer_song_ifs'
+require_relative 'beer_song'
 
 
 
 n = 5000
 Benchmark.bm(7) do |x|
 
-  x.report("procs")   do
+  x.report("lambda")  do
     n.times {
       BeerSong.new.verses(99, 0)
+    }
+  end
+
+  x.report("procs")   do
+    n.times {
+      BeerSongProcs.new.verses(99, 0)
     }
   end
 
@@ -18,11 +25,15 @@ Benchmark.bm(7) do |x|
       BeerSongIFS.new.verses(99, 0)
     }
   end
+
 end
 
 
-# user     system      total        real
-# procs   4.370000   0.000000   4.370000 (  4.381468)
-# ifs     4.240000   0.000000   4.240000 (  4.238254)
+#user     system      total        real
+#
+#lambda   4.250000   0.000000   4.250000 (  4.258555)
+#procs    4.420000   0.000000   4.420000 (  4.419230)
+#ifs      4.260000   0.000000   4.260000 (  4.272677)
+             
 
-#procs aren't too much slower - nice!
+
--- ruby/beer-song/beer_song_procs.rb
+++ ruby/beer-song/beer_song_procs.rb
@@ -1,4 +1,4 @@
-class BeerSong
+class BeerSongProcs
 
   def bottles(x)
     {1=>"1 bottle", 0=>"no more bottles"}[x] || (x.to_s + " bottles")
@@ -6,9 +6,9 @@ class BeerSong
 
   def verse(x)
     result = "#{bottles(x).capitalize} of beer on the wall, #{bottles(x)} of beer.\n"
-    part = {1=> ->{result << "Take it down and pass it around"},
-            0=> ->{result<< "Go to the store and buy some more"; x=100}}
-    part.default = ->{result << "Take one down and pass it around"}
+    part = {1=>Proc.new{result << "Take it down and pass it around"},
+            0=> Proc.new{result<< "Go to the store and buy some more"; x=100}}
+    part.default = Proc.new {result << "Take one down and pass it around"}
     part[x].call
 
     result << ", #{bottles(x-1)} of beer on the wall.\n"
