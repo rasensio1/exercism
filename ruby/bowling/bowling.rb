036643573fc2648d7a46c676642100e1937f021b
Ryan Asensio <ryan.asensio@gmail.com>
2017-10-22 15:13:10 -0500

    make bowling more ruby-like

--- ruby/bowling/bowling.rb
+++ ruby/bowling/bowling.rb
@@ -1,79 +1,73 @@
 class Game
 
   def initialize
-    @frames = [[]]
+    @frames = [Frame.new()]
   end
 
   def roll(pins)
     validate_roll(pins)
-    if last_frame_full?
-      @frames.push([pins])
+    if @frames.last.start_new?
+      @frames << Frame.new(pins) 
     else
-      @frames.last << pins
+      @frames.last.roll(pins)
     end
+    @frames[0...-1].each { |f| f.rolls << pins if f.need_bonus?}
   end
 
   def score
-    validate_score
-    @frames.first(10).map.with_index do |frame, i|
-      score_frame(frame, i)
-    end.sum
+    raise Game::BowlingError if @frames.size < 10
+    @frames.first(10).reduce(0){ |sum, frame| sum + frame.score }
   end
-
-  private
 
   def validate_roll(pins)
-    raise BowlingError.new if [pins.negative?, pins > 10 ].any?
-
-#catches sums for a frame over 10
-    if !last_frame_full? 
-      raise BowlingError.new if @frames.last.sum + pins > 10
-    end
-
-#prevents bonus rolls if 10th frame wasn't strike or spare
-    if @frames.size == 10 && last_frame_full? && @frames.last.sum != 10
-      raise BowlingError.new
-    end
+    raise BowlingError if [ pins > 10, pins < 0,
+                            [@frames.size == 10,
+                             @frames.last.start_new?,
+                             !@frames.last.need_bonus?].all?
+                          ].any?
   end
 
-  def last_frame_full?
-    [@frames.last[0] == 10, @frames.last.size == 2].any?
+  class BowlingError < ArgumentError; end
+
+end
+
+class Frame
+  attr_accessor :rolls
+
+  def initialize(pins=nil)
+    @rolls = (pins ? [pins] : [])
   end
 
-  def validate_score
-    raise BowlingError.new unless @frames.size >=10
-
-   if @frames[9].first == 10 #must roll bonus roles for strike
-     unless @frames.slice(10, 2).flatten.size == 2
-      raise BowlingError.new
-     end
-   end
-
-   if @frames[9].sum == 10 #must roll bonus roles for spare
-      raise BowlingError.new unless @frames.slice(10)
-   end
+  def roll(pins)
+    raise Game::BowlingError if (@rolls[0] || 0) + pins > 10
+    @rolls << pins
   end
 
-  def score_frame(frame, i)
-    if frame.sum < 10
-      frame.sum
-    elsif frame.first == 10 #strike
-      10 + next_throws(2, i)
-    else #spare
-      10 + next_throws(1, i)
-    end
+  def start_new?
+    rolls.first == 10 || size == 2
   end
 
-  def next_throws(number, idx)
-    @frames.slice(idx + 1, 2) #start at next frame, take next two frames
-      .flatten.first(number).reduce(:+)
+  def need_bonus?
+    (strike? || spare?) && size < 3
+  end
+
+  def size
+    rolls.size
+  end
+
+  def strike?
+    rolls.first == 10
+  end
+
+  def spare?
+    !strike? && rolls.first(2).sum == 10
+  end
+
+  def score
+    raise Game::BowlingError if need_bonus?
+    rolls.sum
   end
 
 end
 
-class Game::BowlingError < ArgumentError
-end
-
-module BookKeeping
-  VERSION = 3
-end
+module BookKeeping VERSION = 3; end
