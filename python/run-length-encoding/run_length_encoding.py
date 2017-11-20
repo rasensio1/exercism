8f0c63c8a1a281523eb4d573321f1faa52038958
Ryan Asensio <ryan.asensio@gmail.com>
2017-11-20 12:43:32 -0600

    refactor python run_length"

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -1,34 +1,19 @@
 from itertools import groupby
 import re
 
-def return_empty(f):
-    def wrapper(string):
-        if not string:
-            return string
-        else:
-            return f(string)
-    return wrapper
-
-
-@return_empty
 def decode(string):
-    return "".join(map(expand, re.findall(r'([0-9]*[a-zA-Z\s]{1})', string)))
+    return make_str(expand, re.findall(r'(\d*)([a-zA-Z\s])', string))
 
-@return_empty
 def encode(string):
-    return "".join(map(enc_from_group, groupby(string)))
+    return make_str(enc_group, groupby(string))
 
 def expand(grp):
-    if len(grp) == 1:
-        return grp
-    else:
-        return grp[-1] * int(grp[:-1])
+    return grp[1] * int((grp[0] or 1))
 
-def enc_from_group(grp):
-    size = len(list(grp[1]))
-    let = grp[0]
-    if size == 1:
-        return let
-    else:
-        return str(size) + let
+def enc_group(grp):
+    size, let  = len(list(grp[1])), grp[0]
+    return let if size == 1 else str(size) + let
+
+def make_str(f, group):
+    return "".join(map(f, group))
 
