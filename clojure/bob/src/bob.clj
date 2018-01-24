35da13ad2c7e9469ae38a764a9f3da3d0f2278a4
Ryan Asensio <ryan.asensio@gmail.com>
2018-01-24 15:37:33 -0600

    Refactors bob #clojure

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -3,8 +3,8 @@
 
 (defn response-for [statement]
   (letfn [(shout? [s] (and (re-find #"[A-Z]+" s) (= s (st/upper-case s))))
-          (question? [s] (= \? (last s)))
-          (silence? [s] (empty? (st/replace s #"\s" "")))]
+          (question? [s] (st/ends-with? s "?"))
+          (silence? [s] (st/blank? s))]
     (cond
       (silence? statement) "Fine. Be that way!"
       (shout? statement) "Whoa, chill out!"
